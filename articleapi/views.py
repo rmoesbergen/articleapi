e1cac4640598c6c42805662fbbd7352f639e20e2
Ronald Moesbergen <mail@rmoesbergen.nl>
2018-08-10 20:09:36 +0200

    Implement all missing functions

--- articleapi/views.py
+++ articleapi/views.py
@@ -51,13 +51,7 @@ def v1_articles(request):
     return JsonResponse(data=result)
 
 
-@require_GET
-def v1_article_by_id(request, articleid):
-    try:
-        article = Article.objects.get(pk=articleid)
-    except Article.DoesNotExist:
-        return JsonResponse(data={'result': 'No such article!'})
-
+def v1_get_article_by_id(request, article):
     result = {
         'result': 'ok',
         'article': {
@@ -67,8 +61,46 @@ def v1_article_by_id(request, articleid):
             'author': article.author
         }
     }
-
     return JsonResponse(data=result)
+
+
+@permission_required('articleapi.change_article')
+@json_call
+def v1_post_article_by_id(request, article):
+    params = request.json
+    if 'subject' not in params or 'body' not in params:
+        return JsonResponse(data={'result': 'required field is missing! {0}'.format(params)}, status=401)
+
+    article.subject = params['subject']
+    article.body = params['body']
+    if 'author' in params:
+        article.author = params['author']
+    article.save()
+    return JsonResponse(data={'result': 'ok'})
+
+
+@permission_required('articleapi.delete_article')
+def v1_delete_article_by_id(request, article):
+    article.delete()
+    return JsonResponse(data={'result': 'ok'})
+
+
+@require_http_methods(['GET', 'POST', 'DELETE'])
+def v1_article_by_id(request, articleid):
+    try:
+        article = Article.objects.get(pk=articleid)
+    except Article.DoesNotExist:
+        return JsonResponse(data={'result': 'No such article!'})
+
+    if request.method == 'GET':
+        return v1_get_article_by_id(request, article)
+    elif request.method == 'POST':
+        return v1_post_article_by_id(request, article)
+    elif request.method == 'DELETE':
+        return v1_delete_article_by_id(request, article)
+    else:
+        # Impossible
+        return JsonResponse(data={'result': 'huh?'})
 
 
 @require_http_methods(['PUT'])
